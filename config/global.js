67281f79d33b04f1ee52a60312c550ac1a12e034
Akash414530 <150812698+Akash414530@users.noreply.github.com>
2024-07-03 19:20:29 +0530

    update started

--- config/global.js
+++
@@ -1,3 +0,0 @@
-const API_URL = "https://auth-backend-eqg0.onrender.com";
-
-export default API_URL;
--- src/components/Slogan.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/Slogan.css";
-
-export default function Slogan() {
-  return (
-    <div className="slogan">
-      <p>Game On with Expert Reviews</p>
-    </div>
-  );
-}
